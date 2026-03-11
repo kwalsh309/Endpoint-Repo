32cd9ae189f71c5d09198157ff373e7ec82888f1
kwalsh309 <kwalsh309@gmail.com>
2026-03-11 08:31:55 -0400

    Audit Log script for finding new inbox rules on potentially compromised users

---
+++ Intune/UnifiedAuditLog-InboxRules.ps1
@@ -0,0 +1,19 @@
+$start = (Get-Date).AddDays(-30)
+$end = (Get-Date).AddDays(1)
+$user = "alexandra.koontz@pointpark.edu"
+$report = @()
+
+$audit = Search-UnifiedAuditLog -UserIds $user -StartDate $start -EndDate $end | Where-Object {$_.Operations -eq "New-InboxRule" -or $_.Operations -eq "UpdateInboxRules"}
+
+foreach ($auditlog in $audit)
+{
+    $logentry = [PSCustomObject] @{
+        User = $user
+        RecordType = $auditlog.RecordType
+        CreationDate = $auditlog.CreationDate
+        Operations = $auditlog.Operations
+        AuditData = $auditlog.AuditData
+    }
+    $report += $logentry
+}
+$report | Export-Csv -Path "C:\Users\kwalsh1\Desktop\AuditLogTest.csv" -NoTypeInformation
